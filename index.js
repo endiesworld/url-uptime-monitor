72d82d19281b3fbcc5efac9c68a6f5392b51e2d1
endiesworld <endiesworld@gmail.com>
2021-07-28 12:03:49 +0100

    feat:Required url module, to enable url string parsing

--- index.js
+++ index.js
@@ -1,7 +1,35 @@
 const http = require('http') ;
+const URL = require('url').URL ; 
+const url = require('url') ;
+
+const WORK_ENV = 'test' ;
+
+/**
+ * 
+ * @param {url string} myurl 
+ * @returns url parser
+ */
+const urlParser = (myurl) =>{
+    return (WORK_ENV === 'test') ? url.parse(myurl) : new URL(myurl) ;
+}
 
 //Create an http serve.
 const server = http.createServer((req, res)=>{
+
+    //Get the url and parse it
+    var path = '' ;
+    try{
+        const parsedURL = urlParser(req.url) ;
+        path = parsedURL.pathname ;
+    }catch(error){
+        console.log('url parser error')
+    }
+
+    //Get the path
+    const trimmedPath = path.replace(/^\/+|\/+$/g,'') ;
+
+    console.log('the requested url is: ', {trimmedPath})
+
     res.end('Hello world\n') ;
 })
 
